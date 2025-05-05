e22ebb5f84086638bd06962fb21eb77eda7bab6f
nick <andantej281@gmail.com>
2025-05-05 11:24:27 +0900

    h2 이용한 repository 테스트

--- src/test/java/com/example/demo/repository/UserRepositoryTest.java
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -1,7 +1,65 @@
 package com.example.demo.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-class UserRepositoryTest {
+import com.example.demo.model.UserStatus;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@Sql("/sql/user-repository-test-data.sql")
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void findByIdAndStatus_로_유저_데이터를_찾아올_수_있다() {
+        // given
+        // when
+        Optional<UserEntity> findUser = userRepository.findByIdAndStatus(1,
+            UserStatus.ACTIVE);
+
+        // then
+        assertThat(findUser.isPresent()).isTrue();
+    }
+
+    @Test
+    void findByIdAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
+        // given
+        // when
+        Optional<UserEntity> findUser = userRepository.findByIdAndStatus(2L,
+            UserStatus.ACTIVE);
+
+        // then
+        assertThat(findUser.isEmpty()).isTrue();
+    }
+
+    @Test
+    void findByEmailAndStatus_로_유저_데이터를_찾아올_수_있다() {
+        // given
+        // when
+        Optional<UserEntity> findUser = userRepository.findByEmailAndStatus("kok202@naver.com",
+            UserStatus.ACTIVE);
+
+        // then
+        assertThat(findUser.isPresent()).isTrue();
+    }
+
+    @Test
+    void findByEmailAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
+        // given
+        // when
+        Optional<UserEntity> findUser = userRepository.findByEmailAndStatus("test222@test.com",
+            UserStatus.ACTIVE);
+
+        // then
+        assertThat(findUser.isEmpty()).isTrue();
+    }
 }
---
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (id, email, nickname, address, certification_code, status, last_login_at)
+VALUES (1, 'kok202@naver.com', 'kok202', 'Seoul', 'aaaaaaaa-aaaaa-aaaaa-aaa-aaa-aaaaa', 'ACTIVE', 0);
